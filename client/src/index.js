7cc23f8f32dec0a83f9b058cb122115bc2641d36
Kyrillos Ezzat <kyrillosezzat300@gmail.com>
2023-02-06 15:52:34 +0200

    change api base url

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'
-import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
 
 //https://facebook-clone-mern-stack.herokuapp.com/api/v1/
-axios.defaults.baseURL = 'https://facebook-clone-mern-stack.herokuapp.com/api/v1/';
-axios.defaults.headers = {Authorization:`Bearer ${localStorage.getItem('AccessToken')}`}
+axios.defaults.baseURL =
+  "https://facebook-clone-backend-two.vercel.app/api/v1/";
+axios.defaults.headers = {
+  Authorization: `Bearer ${localStorage.getItem("AccessToken")}`,
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
